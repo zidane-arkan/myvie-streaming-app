0e0f1d247d9bb5f25aae1dc0767f77efd1758055
Zidane Arkan <zidark897@gmail.com>
2025-07-12 14:37:14 +0700

    feat: Slicing Register (SignUp)

--- resources/js/Pages/Prototype/Login.jsx
+++ resources/js/Pages/Prototype/Login.jsx
@@ -2,94 +2,97 @@ import React from "react";
 import InputLabel from "@/Components/InputLabel";
 import Input from "@/Components/Input";
 import Button from "@/Components/Button";
-import { Link } from "@inertiajs/react";
+import { Link, Head } from "@inertiajs/react";
 
 
 function Login() {
     return (
-        <div className="min-h-screen px-3 mx-auto text-white bg-black max-w-screen md:px-10">
-            <div className="fixed top-[-50px] hidden lg:block">
-                <img src="/assets/images/signup-image.png"
-                    className="hidden laptopLg:block laptopLg:max-w-[450px] laptopXl:max-w-[640px]" alt="" />
-            </div>
-            <div className="py-24 flex laptopLg:ml-[680px] laptopXl:ml-[870px]">
-                <div>
-                    <img src="/assets/images/moonton-white.svg" alt="" />
-                    <div className="my-[70px]">
-                        <div className="font-semibold text-[26px] mb-3">
-                            Welcome Back
+        <>
+            <Head title='Sign In' />
+            <div className="min-h-screen px-3 mx-auto text-white bg-black max-w-screen md:px-10">
+                <div className="fixed top-[-50px] hidden lg:block">
+                    <img src="/assets/images/signup-image.png"
+                        className="hidden laptopLg:block laptopLg:max-w-[450px] laptopXl:max-w-[640px]" alt="" />
+                </div>
+                <div className="py-24 flex laptopLg:ml-[680px] laptopXl:ml-[870px]">
+                    <div>
+                        <img src="/assets/images/moonton-white.svg" alt="" />
+                        <div className="my-[70px]">
+                            <div className="font-semibold text-[26px] mb-3">
+                                Welcome Back
+                            </div>
+                            <p className="text-base text-[#767676] leading-7">
+                                Explore our new movies and get <br />
+                                the better insight for your life
+                            </p>
                         </div>
-                        <p className="text-base text-[#767676] leading-7">
-                            Explore our new movies and get <br />
-                            the better insight for your life
-                        </p>
-                    </div>
-                    <form className="w-[370px]">
-                        <div className="flex flex-col gap-6">
-                            <div>
-                                {/* <label className="block mb-2 text-base">Email Address</label> */}
-                                <InputLabel htmlFor="email">Email Address</InputLabel>
-                                {/* <input type="email" name="email"
+                        <form className="w-[370px]">
+                            <div className="flex flex-col gap-6">
+                                <div>
+                                    {/* <label className="block mb-2 text-base">Email Address</label> */}
+                                    <InputLabel htmlFor="email">Email Address</InputLabel>
+                                    {/* <input type="email" name="email"
                                     className="rounded-2xl bg-form-bg py-[13px] px-7 w-full focus:outline-alerange focus:outline-none"
                                     placeholder="Email Address" /> */}
-                                <Input
-                                    id="email"
-                                    type="email"
-                                    name="email"
-                                    autoComplete="username"
-                                    placeholder="Email Address"
-                                    isFocused={true}
-                                    required={true}
-                                />
+                                    <Input
+                                        id="email"
+                                        type="email"
+                                        name="email"
+                                        autoComplete="username"
+                                        placeholder="Email Address"
+                                        isFocused={true}
+                                        required={true}
+                                    />
 
-                            </div>
-                            <div>
-                                {/* <label className="block mb-2 text-base">Password</label> */}
-                                <InputLabel htmlFor="password">Password</InputLabel>
-                                {/* <input type="password" name="password"
+                                </div>
+                                <div>
+                                    {/* <label className="block mb-2 text-base">Password</label> */}
+                                    <InputLabel htmlFor="password">Password</InputLabel>
+                                    {/* <input type="password" name="password"
                                     className="rounded-2xl bg-form-bg py-[13px] px-7 w-full focus:outline-alerange focus:outline-none"
                                     placeholder="Password" /> */}
-                                <Input
-                                    id="password"
-                                    type="password" name="password"
-                                    className="rounded-2xl bg-form-bg py-[13px] px-7 w-full focus:outline-alerange focus:outline-none"
-                                    placeholder="Password"
-                                />
+                                    <Input
+                                        id="password"
+                                        type="password"
+                                        name="password"
+                                        placeholder="Password"
+                                    />
+                                </div>
                             </div>
-                        </div>
-                        <div className="grid space-y-[14px] mt-[30px]">
-                            {/* <a href="/" className="rounded-2xl bg-alerange py-[13px] text-center">
+                            <div className="grid space-y-[14px] mt-[30px]">
+                                {/* <a href="/" className="rounded-2xl bg-alerange py-[13px] text-center">
                                 <span className="text-base font-semibold">
                                     Start Watching
                                 </span>
                             </a> */}
-                            <Button type="button" variant="primary" disabled={false}>
-                                <span className="text-base font-semibold">
-                                    Start Watching
-                                </span>
-                            </Button>
-                            {/* <a href="sign_up.html" className="rounded-2xl border border-white py-[13px] text-center">
+                                <Button type="button" variant="primary" disabled={false}>
+                                    <span className="text-base font-semibold">
+                                        Start Watching
+                                    </span>
+                                </Button>
+                                {/* <a href="sign_up.html" className="rounded-2xl border border-white py-[13px] text-center">
                                 <span className="text-base text-white">
                                     Create New Account
                                 </span>
                             </a> */}
-                            <Link href={route("prototype.register")}>
-                                <Button type="button" variant="light-outline" disabled={false}>
-                                    <span className="text-base font-semibold">
-                                        Create New Account
-                                    </span>
-                                </Button>
-                            </Link>
-                            {/* <!-- <button type="submit" className="rounded-2xl bg-alerange py-[13px] text-center">
+                                <Link href={route("prototype.register")}>
+                                    <Button type="button" variant="light-outline" disabled={false}>
+                                        <span className="text-base">
+                                            Create New Account
+                                        </span>
+                                    </Button>
+                                </Link>
+                                {/* <!-- <button type="submit" className="rounded-2xl bg-alerange py-[13px] text-center">
                                     <span className="text-base font-semibold">
                                         Start Watching
                                     </span>
                                 </button> --> */}
-                        </div>
-                    </form>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
--- resources/js/Pages/Prototype/Register.jsx
+++ resources/js/Pages/Prototype/Register.jsx
@@ -1,8 +1,114 @@
 import React from 'react'
+import InputLabel from '@/Components/InputLabel';
+import Input from '@/Components/Input';
+import Button from '@/Components/Button';
+import { Link, Head } from '@inertiajs/react';
+
 
 function Register() {
     return (
-        <div>Register</div>
+        <>
+            <Head title='Sign Up' />
+            <div className="mx-auto max-w-screen min-h-screen bg-black text-white md:px-10 px-3">
+                <div className="fixed top-[-50px] hidden lg:block">
+                    <img src="/assets/images/signup-image.png"
+                        className="hidden laptopLg:block laptopLg:max-w-[450px] laptopXl:max-w-[640px]" alt="" />
+                </div>
+                <div className="py-24 flex laptopLg:ml-[680px] laptopXl:ml-[870px]">
+                    <div>
+                        <img src="/assets/images/moonton-white.svg" alt="" />
+                        <div className="my-[70px]">
+                            <div className="font-semibold text-[26px] mb-3">
+                                Sign Up
+                            </div>
+                            <p className="text-base text-[#767676] leading-7">
+                                Explore our new movies and get <br />
+                                the better insight for your life
+                            </p>
+                        </div>
+                        <form className="w-[370px]">
+                            <div className="flex flex-col gap-6">
+                                <div>
+                                    {/* <label className="text-base block mb-2">Full Name</label> */}
+                                    <InputLabel htmlFor="fullname" value="Full Name" />
+                                    {/* <input type="text" name="fullname"
+                                    className="rounded-2xl bg-form-bg py-[13px] px-7 w-full focus:outline-alerange focus:outline-none"
+                                    placeholder="Your fullname..." value="Angga React" /> */}
+                                    <Input
+                                        id="fullname"
+                                        type="text"
+                                        name="fullname"
+                                        placeholder="Your fullname..."
+                                        isFocused={false}
+                                        required={true}
+                                    />
+                                </div>
+                                <div>
+                                    {/* <label className="text-base block mb-2">Email Address</label> */}
+                                    <InputLabel htmlFor="email" value="Email Address" />
+                                    {/* <input type="email" name="email"
+                                    className="rounded-2xl bg-form-bg py-[13px] px-7 w-full focus:outline-alerange focus:outline-none"
+                                    placeholder="Your Email Address" value="anggaforreact@fb.com" /> */}
+                                    <Input
+                                        id="email"
+                                        type="email"
+                                        name="email"
+                                        placeholder="Your Email Address"
+                                        autoComplete="email"
+                                        isFocused={false}
+                                        required={true}
+                                    />
+                                </div>
+                                <div>
+                                    {/* <label className="text-base block mb-2">Password</label> */}
+                                    <InputLabel htmlFor="password" value="Password" />
+                                    {/* <input type="password" name="password"
+                                    className="rounded-2xl bg-form-bg py-[13px] px-7 w-full focus:outline-alerange focus:outline-none"
+                                    placeholder="Your Password" value="eeeeeeeeeeeeeee" /> */}
+                                    <Input
+                                        id="password"
+                                        type="password"
+                                        name="password"
+                                        placeholder="Your Password"
+                                        isFocused={false}
+                                        required={true}
+                                    />
+                                </div>
+                            </div>
+                            <div className="grid space-y-[14px] mt-[30px]">
+                                {/* <a href="/" className="rounded-2xl bg-alerange py-[13px] text-center">
+                                <span className="text-base font-semibold">
+                                    Sign Up
+                                </span>
+                            </a> */}
+                                <Button variant='primary' type='button'>
+                                    <span className="text-base font-semibold">
+                                        Sign Up
+                                    </span>
+                                </Button>
+                                {/* <a href="sign_in.html" className="rounded-2xl border border-white py-[13px] text-center">
+                                <span className="text-base text-white">
+                                    Sign In to My Account
+                                </span>
+                            </a> */}
+                                <Link href={route("prototype.login")}>
+                                    <Button variant='white-outline' type="button">
+                                        <span className="text-base text-white">
+                                            Sign In to My Account
+                                        </span>
+                                    </Button>
+                                </Link>
+                                {/* <!-- <button type="submit" className="rounded-2xl bg-alerange py-[13px] text-center">
+                            <span className="text-base font-semibold">
+                                Sign Up
+                            </span>
+                        </button> --> */}
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
